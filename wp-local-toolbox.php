955de9aba3df5a3b483e512360b29b868061e6f5
Joe Guilmette <joe.guilmette@gmail.com>
2015-03-16 08:22:55 +0700

    code readability and comments

--- wp-local-toolbox.php
+++ wp-local-toolbox.php
@@ -7,6 +7,67 @@ Version: 1.0
 Author URI: http://joeguilmette.com
 */
 
+if (defined('WPLT_ENVIRONMENT') && WPLT_ENVIRONMENT ) {
+
+	/*
+	You can edit this to do certain things depending on your how you've
+	defined the WPLT_ENVIRONMENT constant. This can be very useful if
+	you want to perform certain actions depending on which server you're
+	using. 
+
+	If you come up with something cool I'd love a pull request!
+	*/ 
+	if (strtoupper(WPLT_ENVIRONMENT) != 'LIVE' && strtoupper(WPLT_ENVIRONMENT) != 'PRODUCTION') {
+		// Everything except PRODUCTION/LIVE Environment
+
+		// Hide from robots
+		add_filter( 'pre_option_blog_public', '__return_zero' );
+
+	} else {
+		// PRODUCTION/LIVE Environment
+
+	}
+
+	// Disable plugins
+	if (defined('WPLT_DISABLED_PLUGINS') && WPLT_DISABLED_PLUGINS ) {
+		new WPLT_Disable( unserialize (WPLT_DISABLED_PLUGINS) );
+	}
+
+	// Add admin notice
+	function environment_notice() {
+		$env_text = strtoupper(WPLT_ENVIRONMENT);
+
+		$admin_notice = array(
+			'parent'	=> 'top-secondary', // puts it on the right side.
+			'id'		=> 'environment-notice',
+			'title'		=> '<span>'.$env_text.' SERVER</span>',
+		);
+		global $wp_admin_bar;
+		$wp_admin_bar->add_menu($admin_notice);
+	}
+
+	// Style the admin bar
+	function environment_notice_css() {
+
+		if (defined( 'WPLT_COLOR' ) && WPLT_COLOR) {
+			$env_color = strtolower(WPLT_COLOR);
+		} else {
+			$env_color = 'red';
+		}
+
+		echo "
+		<style type='text/css'>#wp-admin-bar-environment-notice>div,#wpadminbar{background-color:$env_color!important}#wp-admin-bar-environment-notice>div>span{color:#EEEFE6!important;font-size:20px!important}.ab-icon:before,.ab-item:before,#adminbarsearch:before{color:#EEEFE6!important}</style>";
+	}
+
+	// Add the environment to the admin panel
+	add_action( 'admin_bar_menu', 'environment_notice' );
+
+	// Add CSS to admin and wp head
+	add_action( 'admin_head', 'environment_notice_css' );
+	add_action( 'wp_head', 'environment_notice_css' );
+
+}
+
 class WPLT_Disable {
 	// Author: Mark Jaquith
 	// Author URI: http://coveredwebservices.com/
@@ -52,59 +113,6 @@ class WPLT_Disable {
 			}
 		}
 		return $plugins;
-	}
-}
-
-if (defined('WPLT_ENVIRONMENT') && WPLT_ENVIRONMENT ) {
-
-	// Add admin notice
-	function environment_notice() {
-		$env_text = strtoupper(WPLT_ENVIRONMENT);
-
-		$admin_notice = array(
-			'parent'	=> 'top-secondary', // puts it on the right side.
-			'id'		=> 'environment-notice',
-			'title'		=> '<span>'.$env_text.' SERVER</span>',
-		);
-		global $wp_admin_bar;
-		$wp_admin_bar->add_menu($admin_notice);
-	}
-
-	// Style the admin bar
-	function environment_notice_css() {
-
-		if (defined( 'WPLT_COLOR' ) && WPLT_COLOR) {
-			$env_color = strtolower(WPLT_COLOR);
-		} else {
-			$env_color = 'red';
-		}
-
-		echo "
-		<style type='text/css'>#wp-admin-bar-environment-notice>div,#wpadminbar{background-color:$env_color!important}#wp-admin-bar-environment-notice>div>span{color:#EEEFE6!important;font-size:20px!important}.ab-icon:before,.ab-item:before,#adminbarsearch:before{color:#EEEFE6!important}</style>";
-	}
-
-	// Add the environment to the admin panel
-	// add_action( 'admin_notices', 'environment_notice' );
-
-	add_action( 'admin_bar_menu', 'environment_notice' );
-
-	// Add CSS to admin and wp head
-	add_action( 'admin_head', 'environment_notice_css' );
-	add_action( 'wp_head', 'environment_notice_css' );
-
-	if (defined('WPLT_DISABLED_PLUGINS') && WPLT_DISABLED_PLUGINS ) {
-		new WPLT_Disable( unserialize (WPLT_DISABLED_PLUGINS) );
-	}
-
-	if (strtoupper(WPLT_ENVIRONMENT) != 'LIVE' && strtoupper(WPLT_ENVIRONMENT) != 'PRODUCTION') {
-		// EVERYTHING EXCEPT PRODUCTION/LIVE ENVIRONMENT
-
-		// Hide from robots
-		add_filter( 'pre_option_blog_public', '__return_zero' );
-
-	} else {
-		// PRODUCTION/LIVE ENVIRONMENT
-
 	}
 }
 
