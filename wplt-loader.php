101cbb8837734fdeac4d439a055de389ed2b31fb
Joe Guilmette <joe.guilmette@gmail.com>
2015-04-23 12:38:38 +0200

    change plugin header back to WPLT

---
+++ wplt-loader.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WP Local Toolbox
+Description: A simple plugin to set different defaults for production, staging and local servers.
+Author: Joe Guilmette
+Version: 1.1
+Author URI: http://joeguilmette.com
+Plugin URI: https://github.com/joeguilmette/wp-local-toolbox
+License: GPLv2+
+ */
+
+$required_php_version = '5.3.0';
+if (version_compare(phpversion(), $required_php_version, '>')) {
+	require_once __DIR__ . '/toolbox/wp-local-toolbox.php';
+}
