eae6a32c25ebfb3dbefbe09da1a7aad0fba2d7fc
Jon Brown <jb510@users.noreply.github.com>
2022-11-15 11:07:23 -0800

    Update wplt-loader.php
    Bumping version to 1.3.1 for airplane mode compatibility with PHP 8.  Upping the minimum PHP version to 7.0 just because...  This whole plugin needs a major overhaul, but presently still works, just nobody it going to test it < 7 going forward.

--- wplt-loader.php
+++ wplt-loader.php
@@ -3,14 +3,14 @@
 Plugin Name: WP Local Toolbox
 Description: A simple plugin to set different defaults for production, staging and local servers.
 Author: Joe Guilmette
-Version: 1.3.0
+Version: 1.3.1
 Author URI: http://joeguilmette.com
 Plugin URI: https://github.com/joeguilmette/wp-local-toolbox
 License: GPLv2+
 Text Domain: wp-local-toolbox
  */
 
-$required_php_version = '5.3.0';
+$required_php_version = '7.0.0';
 if (version_compare(phpversion(), $required_php_version, '>')) {
 	require_once __DIR__ . '/toolbox/wp-local-toolbox.php';
 }
